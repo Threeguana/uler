991743e40c9d03d80685d49e4ca6cfe7d0b13221
Andini Tribuana <threebuanandini@gmail.com>
2025-12-04 23:35:15 +0700

    add bindings

--- uler.py
+++ uler.py
@@ -19,13 +19,45 @@ kepala.shape("triangle") #bentuk kepala segitiga dengan warna meraj
 kepala.color("red")
 kepala.penup() #matikan pen
 kepala.goto(0,0) #posisi kepala di tengha layar
-kepala.direction = "up"
+kepala.direction = "stop"
 
 # function
+def keatas():
+    kepala.direction = "up"
+
+def kebawah():
+    kepala.direction = "down"
+
+def kekiri():
+    kepala.direction = "left"
+
+def kekanan():
+    kepala.direction = "right"
+
+
 def gerak():
     if kepala.direction == "up": #cek jika arah ke atas
         y = kepala.ycor() #ambil koordinat y
         kepala.sety(y + 20) #geser kepala ke atas sebanyak 20px
+
+    if kepala.direction == "down":
+        y = kepala.ycor()
+        kepala.sety(y - 20)
+
+    if kepala.direction == "left":
+        x = kepala.xcor()
+        kepala.setx(x - 20)
+
+    if kepala.direction == "right":
+        x = kepala.xcor()
+        kepala.setx(x + 20)
+
+# keyboard binding
+window.listen()
+window.onkeypress(keatas, "w")
+window.onkeypress(kebawah, "s")
+window.onkeypress(kekiri, "a")
+window.onkeypress(kekanan, "d")
 
 # main loop
 while True:
